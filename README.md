ce532bd939000ddfd16b6a1b00c6da796b4eccb4
SergePlaski <splaski@gmail.com>
2023-10-08 21:37:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Course Progress Tracker App (Vanilla JS Version)
+
+This is a simple app that tracks my progress in my Ract 2023 course. It is built using Vanilla JS and Chart.JS library as my main goal was to brush up and practice the following topics: 
+
+- Array methods
+- String operations
+- DOM manipulation
+- Handling DOM events & Event delegation
+- State management with localStorage
+- CSS Grid and Flexbox
+
+Chart.js Documentation and example:
+https://www.chartjs.org/docs/latest/charts/doughnut.html
+https://codepen.io/benlor/pen/KKpdvVX
+
+## TODO: 
+- Convert to React components
+- Add ability to add and store my notes and code snippets using Firebase or other backend API
--- course.js
+++ course.js
@@ -78,27 +78,143 @@ const modules = [
 ];
 
 function convertToMin(hm) {
-  return hm.split(":").reduce((a, b) => +a * 60 + +b, 0);
+	return hm.split(":").reduce((a, b) => +a * 60 + +b, 0);
 }
 
 function convertToHrsMin(m) {
-  const hrs = Math.floor(m/60);
-  const mins = m - hrs *60;
-  return hrs + ':' + mins;
+	const hrs = Math.floor(m / 60);
+	const mins = m - hrs * 60;
+	return hrs.toString() + ":" + ("0" + mins.toString()).slice(-2);
 }
 
 function getTotalDuration(arr) {
-  return arr.reduce((a,b) => a + b, 0);
+	return arr.reduce((a, b) => a + b, 0);
 }
 
-const completedIndex = 8;
-console.log(modules[completedIndex]);
+function renderChart(chartId, labelId, completedPct) {
+	const ctx = document.getElementById(chartId);
+	const label = document.getElementById(labelId);
+	new Chart(ctx, {
+		type: "doughnut",
+		data: {
+			// labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+			labels: [],
+			datasets: [
+				{
+					label: "my progress",
+					data: [completedPct, 100 - completedPct],
+					backgroundColor: [
+						// 'rgba(255, 99, 132, 0.5)',
+						// 'rgba(54, 162, 235, 0.2)',
+						// 'rgba(255, 206, 86, 0.2)',
+						// 'rgba(75, 192, 192, 0.2)'
+						"rgb(24, 71, 109)",
+						"rgba(24, 71, 109, 0.3)",
+					],
+					borderColor: [
+						// 'rgba(255,99,132,1)',
+						// 'rgba(54, 162, 235, 1)',
+						// 'rgba(255, 206, 86, 1)',
+						// 'rgba(75, 192, 192, 1)'
+					],
+					borderWidth: 1,
+				},
+			],
+		},
+		options: {
+			cutoutPercentage: 70,
+			responsive: false,
+		},
+	});
 
-const allModulesDurations = modules.map(module => convertToMin(module.duration));
-const completedModulesDurations = allModulesDurations.slice(0, completedIndex + 1);
-const completedPct = (getTotalDuration(completedModulesDurations) * 100 / getTotalDuration(allModulesDurations)).toFixed(1);
+	label.textContent = completedPct + "%";
+}
 
-document.getElementById('total').textContent = convertToHrsMin(getTotalDuration(allModulesDurations));
-document.getElementById('done').textContent = convertToHrsMin(getTotalDuration(completedModulesDurations));
-document.getElementById('progress').textContent = completedPct + "%";
-document.getElementById('progressbar').setAttribute('value', completedPct);
+function renderModulesData(id) {
+	let el = document.getElementById(id);
+	modules.forEach((module, index) => {
+		const m = module.title.split(" - ");
+		el.innerHTML += `<div class='module ${
+			module.duration === "0:00" ? "optional" : ""
+		}' key=${index+1}>
+<p class='module__number'>${m[0]}</p>
+<p class='module__title'>${m[1]}</p>
+<p class='module__duration'>${module.duration}</p></div>`;
+	});
+
+	// return NodeList of module elements
+	return document.querySelectorAll(".module");
+}
+
+// calculate course progress
+function updateProgressStats(completedModuleIndex = 0) {
+	console.log('completed ' + completedModuleIndex);
+
+	// calculate tital time
+	const allModulesDurations = modules.map((module) =>
+	convertToMin(module.duration)
+	);
+	const totalDuration = getTotalDuration(allModulesDurations);
+	// initial values
+	let completedDuration = 0;
+	let completedPct = 0;
+	
+	if (completedModuleIndex > 0) { 
+		// some modules have been completed - calculate completed time/pct
+		const completedModulesDurations = allModulesDurations.slice(
+			0,
+			completedModuleIndex
+		);
+		completedDuration = getTotalDuration(completedModulesDurations);
+		completedPct = Math.round(
+			(getTotalDuration(completedModulesDurations) * 100) /
+				getTotalDuration(allModulesDurations)
+		);
+
+		// highlight all completed modules
+		moduleNodes.forEach((moduleNode) => {
+			if (+moduleNode.getAttribute("key") <= completedModuleIndex) {
+				moduleNode.classList.add("selected");
+			} else {
+				moduleNode.classList.remove("selected");
+			}
+		});
+	}
+
+	// reflect completed stats on the UI and chart
+	document.getElementById("total").textContent = convertToHrsMin(totalDuration);
+	document.getElementById("compl").textContent =
+		convertToHrsMin(completedDuration);
+	renderChart("myChart", "myProgress", completedPct);
+}
+
+function clearCurrentSelection() {
+	moduleNodes.forEach((module) => module.classList.remove("selected"));
+}
+
+// ***********  initialize the app **************
+const moduleNodes = renderModulesData("modules");
+// check for stored selection and update the progress stats
+const selectedModuleIndex = localStorage.getItem("selectedModuleIndex");
+
+updateProgressStats(+selectedModuleIndex);
+
+// wire up modules for click event
+moduleNodes.forEach((el) => {
+	el.addEventListener("click", (e) => {
+		try {
+			const selected_module = e.target.closest(".module");
+			if (selected_module) {
+				const selectedModuleIndex = +selected_module.getAttribute("key");
+				// store the selection
+				localStorage.setItem("selectedModuleIndex", selectedModuleIndex);
+				// update progress stats
+				updateProgressStats(selectedModuleIndex);
+			} else {
+				throw new Error("No module selected");
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	});
+});
